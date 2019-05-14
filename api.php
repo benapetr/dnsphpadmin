032c53cb0c239bfebab09909b663592bd0c46df6
Petr Bena <benapetr@gmail.com>
2019-05-14 10:25:32 +0200

    it's better to disable debug on prod

--- api.php
+++ api.php
@@ -11,9 +11,9 @@
 // GNU General Public License for more details.
 
 // This is useful for debugging
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+//ini_set('display_errors', 1);
+//ini_set('display_startup_errors', 1);
+//error_reporting(E_ALL);
 
 define('G_DNSTOOL_ENTRY_POINT', 'api.php');
 
