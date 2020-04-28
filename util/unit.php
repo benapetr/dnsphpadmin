98a879ddec9a8bc2c979e25d2afcd61ebf22cc02
Petr Bena <benapetr@gmail.com>
2020-04-28 23:56:23 +0200

    more unit tests

--- util/unit.php
+++ util/unit.php
@@ -45,11 +45,14 @@ $ut->Evaluate('Get zone for FQDN', Zones::GetZoneForFQDN('0.0.168.192.in-addr.ar
 
 $dz1 = raw_zone_to_array(file_get_contents(dirname(__FILE__) . '/testdata/valid.zone1'));
 $dz2 = raw_zone_to_array(file_get_contents(dirname(__FILE__) . '/testdata/invalid.zone'));
+$dz3 = raw_zone_to_array(file_get_contents(dirname(__FILE__) . '/testdata/valid.zone2'));
 
 $ut->Evaluate('Check validness of valid zone testdata/valid.zone1', CheckIfZoneIsComplete($dz1) === true);
 $ut->Evaluate('Check validness of invalid zone testdata/invalid.zone', CheckIfZoneIsComplete($dz2) === false);
+$ut->Evaluate('Check validness of valid zone testdata/valid.zone2', CheckIfZoneIsComplete($dz3) === true);
 $ut->Evaluate('Check count of records in testdata/valid.zone1', count($dz1) === 389);
-$ut->Evaluate('Parser test', CheckZone($dz1));
+$ut->Evaluate('Parser test - zone 1', CheckZone($dz1));
+$ut->Evaluate('Parser test - zone 2', CheckZone($dz3));
 
 echo ("\n");
 $ut->PrintResults();
