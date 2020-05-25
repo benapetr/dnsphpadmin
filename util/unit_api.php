2564cd90e1f9e47f2eed1a48f3fa67892e3132ea
Petr Bena <benapetr@gmail.com>
2020-05-25 13:05:31 +0200

    improved API unit test

--- util/unit_api.php
+++ util/unit_api.php
@@ -27,8 +27,30 @@ $ut->Evaluate("get_version", $version['version'] == G_DNSTOOL_VERSION);
 $logged = api("action=is_logged");
 $ut->Evaluate("is_logged", $logged['is_logged']);
 
+$list = api("action=list_zones");
+//var_export($list);
+$ut->Evaluate("list_zones contains local test", array_key_exists('test.local', $list));
+$ut->Evaluate("list_zones contains update server localhost", $list['test.local']['update_server'] == 'localhost');
+$ut->Evaluate("list_zones contains transfer server localhost", $list['test.local']['transfer_server'] == 'localhost');
+
+$zone_transfer = api("action=list_records&zone=test.local");
+$ut->Evaluate("list_records contains SOA for local", $zone_transfer[0][3] == "SOA");
+//var_export($zone_transfer);
+
+$output = api('action=create_record&record=test1.test.local&ttl=10&type=A&value=10.2.2.8');
+$ut->Evaluate('create_record test A', $output['result'] == 'success');
+
+$record = api('action=get_record&record=test1.test.local');
+$ut->Evaluate('get_record TTL', $record[0][1] == '10');
+$ut->Evaluate('get_record value', $record[0][4] == '10.2.2.8');
+$ut->Evaluate('get_record name', $record[0][0] == 'test1.test.local.');
+
+$output = api('action=delete_record&record=test1.test.local&ttl=10&type=A&value=10.2.2.8');
+$ut->Evaluate('delete_record test A', $output['result'] == 'success');
+
 $login = api("action=logout");
 $ut->Evaluate("logout", $login['result'] == 'success');
+
 
 
 echo ("\n\n\n");
